5b4e09c2661ca32d6a643ae31e23fbe3cec65487
Matheus Acfer <matheus.acfer@nave.org.br>
2020-04-15 18:16:24 -0300

    Finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        return transaction.value;
+      }
+
+      return 0;
+    });
+
+    const outcomes = this.transactions.map(transaction => {
+      if (transaction.type === 'outcome') {
+        return transaction.value;
+      }
+
+      return 0;
+    });
+
+    const incomesTotal = incomes.reduce((total, value) => total + value, 0);
+    const outcomesTotal = outcomes.reduce((total, value) => total + value, 0);
+
+    const total = incomesTotal - outcomesTotal;
+
+    const balance = {
+      income: incomesTotal,
+      outcome: outcomesTotal,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
